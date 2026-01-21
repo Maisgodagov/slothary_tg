87b583090ef0d2bd8f00c1953a56c465d76e6445
Mais Godagov <mgodagov@vk.com>
2026-01-21 22:10:56 +0300

    sdafewgr

--- src/modules/video/container/Video/styles.ts
+++ src/modules/video/container/Video/styles.ts
@@ -1,8 +1,9 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const VideoWrapper = styled.div`
   display: flex;
   flex: 1;
   height: 100%;
   width: 100%;
+  justify-content: center;
 `;
